dbeb223879ef5c21d13ed8573b8362c524221173
xenofiodor <fedorovxa@gmail.com>
2020-01-20 23:54:01 +0100

    Shift animation added

--- ColorParser.java
+++ ColorParser.java
@@ -1,12 +1,7 @@
 package ascii_art;
 
 public class ColorParser {
-    private char upTo128 = ' ';
-    private char upTo255 = '-';
-    private char upTo383 = '+';
-    private char upTo510 = '=';
-    private char upTo638 = '%';
-    private char upTo765 = '#';
+    private char [] symbols = {' ', '-', '+', '=', '%', '#'};
 
     public char[][] RGBSumToChars(int[][] colors){
         int width = colors[0].length;
@@ -15,17 +10,17 @@ public class ColorParser {
         for (int x = 0; x < height; x++){
             for (int y = 0; y < width; y++){
                 if(colors[x][y] >= 638)
-                    chars[x][y] = this.upTo765;
+                    chars[x][y] = this.symbols[5];
                 else if(colors[x][y] >= 510)
-                    chars[x][y] = this.upTo638;
+                    chars[x][y] = this.symbols[4];
                 else if(colors[x][y] >= 383)
-                    chars[x][y] = this.upTo510;
+                    chars[x][y] = this.symbols[3];
                 else if(colors[x][y] >= 255)
-                    chars[x][y] = this.upTo383;
+                    chars[x][y] = this.symbols[2];
                 else if(colors[x][y] >= 128)
-                    chars[x][y] = this.upTo255;
+                    chars[x][y] = this.symbols[1];
                 else
-                    chars[x][y] = this.upTo128;
+                    chars[x][y] = this.symbols[0];
 
             }
         }
@@ -47,4 +42,39 @@ public class ColorParser {
         }
         return String.valueOf(string);
     }
+
+    private int getIndexOf (char c){
+        for (int i = 0; i < 6; i++){
+            if (this.symbols[i] == c)
+                return i;
+        }
+        return -1;
+    }
+
+    public char[][][] fillPicStorage(char[][] pic){
+        int width = pic[0].length;
+        int height = pic.length;
+        char[][][] picStorage = new char[6][height][width];
+        picStorage[0] = pic;
+        for (int i = 1; i <= 5; i++){
+            for (int x = 0; x < height; x++){
+                for (int y = 0; y < width; y++){
+                    picStorage[i][x][y] = this.symbols[(this.getIndexOf(pic[x][y]) + i) % 6];
+                }
+            }
+        }
+        return picStorage;
+    }
+
+    public static void animate(char[][][] picStorage) throws InterruptedException {
+        for (int i = 0; i < 6; i++){
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            System.out.println(charsToString(picStorage[i]));
+            Thread.sleep(2000);
+        }
+    }
+
+
+
 }
--- Main.java
+++ Main.java
@@ -2,13 +2,16 @@ package ascii_art;
 
 import java.io.IOException;
 
+import static ascii_art.ColorParser.animate;
 import static ascii_art.ImageParser.imageToRGBSum;
 import static ascii_art.ColorParser.charsToString;
 
 public class Main {
-    public static void main(String [] args) throws IOException {
+    public static void main(String [] args) throws IOException, InterruptedException {
         ColorParser colorParser = new ColorParser();
-        System.out.println(charsToString(colorParser.RGBSumToChars(
-                imageToRGBSum("/home/xeno/xWs/Java/ascii-art/src/ascii_art/horses.jpg"))));
+//        System.out.println(charsToString(colorParser.RGBSumToChars(
+//                imageToRGBSum("/home/xeno/xWs/Java/ascii-art/src/ascii_art/
+        animate(colorParser.fillPicStorage(colorParser.RGBSumToChars(
+                imageToRGBSum("/home/xeno/xWs/Java/ascii-art/src/ascii_art/small_horse.png"))));
     }
 }
